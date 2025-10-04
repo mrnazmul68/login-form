9fd9f334568f511086be025c9cc293513463b272
mrnazmul68 <mrnazmul68@gmail.com>
2025-10-04 13:45:30 +0600

    Add files via upload

---
+++ login_register/user_page.php
@@ -0,0 +1,26 @@
+<?php
+
+session_start();
+if (!isset($_SESSION['email'])) {
+    header("location: index.php");
+    exit();
+    
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User page</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body style="background: #fff;">
+    <div class="box">
+        <h1>Wecome,<span><?= $_SESSION['name']; ?></span></h1>
+        <p>this is an <span>user</span>page</p>
+        <button onclick="window.location.hrf='logout.php'";>Logout</button>
+    </div>
+</body>
+</html>
